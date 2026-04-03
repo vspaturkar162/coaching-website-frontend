846987b70b7966ed669bcf28ef73377d74fe67dd
vaishnavi paturkar <vaishanavipaturkar162@gmail.com>
2026-04-03 12:37:13 +0530

    toggle props

--- src/admin/components/AdminLayout.tsx
+++ src/admin/components/AdminLayout.tsx
@@ -84,7 +84,7 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
       {/* Main Content */}
       <div className="flex flex-col flex-1 overflow-hidden">
 
-        <AdminNavbar toggleSidebar={toggleSidebar} />
+        <AdminNavbar  />
 
         <main className="flex-1 overflow-y-auto p-4 md:p-8">
 
