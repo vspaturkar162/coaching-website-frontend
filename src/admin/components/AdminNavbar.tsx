d67a4c5b24ab4106ac6245657b53d316d79f5720
vaishnavi paturkar <vaishanavipaturkar162@gmail.com>
2026-04-03 12:34:21 +0530

    toggle button removed from navbar

--- src/admin/components/AdminNavbar.tsx
+++ src/admin/components/AdminNavbar.tsx
@@ -1,21 +1,21 @@
 
-import { Menu } from "lucide-react"
+// import { Menu } from "lucide-react"
 import { Search, Bell, User } from "lucide-react"
-interface AdminNavbarProps {
-  toggleSidebar: () => void
-}
+// interface AdminNavbarProps {
+//   toggleSidebar: () => void
+// }
 
-export default function AdminNavbar({ toggleSidebar }: AdminNavbarProps) {
+export default function AdminNavbar() {
 
   return (
     <header className="bg-gradient-to-r border-blue-700 bg-transperant shadow-sm px-6 py-4 flex items-center justify-between">
       {/* Sidebar Toggle Button */}
-      <button
+      {/* <button
         onClick={toggleSidebar}
         className="p-2 rounded-lg hover:bg-blue-100 transition"
       >
         <Menu className="text-blue-700" />
-      </button>
+      </button> */}
       {/* Search */}
       <div className="flex items-center gap-2 bg-gray-50 px-3 py-2 rounded-lg w-80">
         <Search size={16} />
